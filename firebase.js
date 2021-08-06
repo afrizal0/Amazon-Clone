bdba30865de57f91e690d70e4a0698c51654fd04
afrizal0 <wikimedicx4504@gmail.com>
2021-08-06 19:29:01 +0800

    add a shopping cart item, total cost, the number template, button complete order, and set up the firebase.js

---
+++ firebase.js
@@ -0,0 +1,23 @@
+var firebaseConfig = {
+
+    apiKey: "AIzaSyASw8Wg3oI0WPt-n0nGXLJGp6ysAYyxHe0",
+
+    authDomain: "clone-4e5c2.firebaseapp.com",
+
+    projectId: "clone-4e5c2",
+
+    storageBucket: "clone-4e5c2.appspot.com",
+
+    messagingSenderId: "505533255556",
+
+    appId: "1:505533255556:web:931c078b944d794c75d7d1",
+
+    measurementId: "G-D076QBV57J"
+
+  };
+
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+  firebase.analytics();
